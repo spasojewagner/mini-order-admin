2b9dcf3b89f6613c785044e9ecf87594199cca70
spasojewagner <markospasojevic758@gmail.com>
2026-07-02 19:46:17 +0200

    feat: add product CRUD with price and stock validation

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('customers', CustomerController::class);
+Route::resource('products', ProductController::class);
